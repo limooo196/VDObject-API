cda19f15ed1b418fdfcba3350c5ea4cb70655f36
limooo196 <93628886+limooo196@users.noreply.github.com>
2022-11-07 00:05:11 +0800

    createConnection to createPool

--- db.js
+++ db.js
@@ -2,16 +2,12 @@ const mysql = require("mysql2");
 //const mysqlPromise =require('mysql2/promise');
 require("dotenv").config();
 
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
 });
 
-connection.connect((err) => {
-  let message = !err ? "Connected" : "Failed to connect";
-  console.log(`mysql : ${message}`);
-});
 
 module.exports = connection;
